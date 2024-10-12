fb9b1e8dd9b89d08a3e8bda340ca91545c9f905a
Oldřich Machala <xmachal2@fi.muni.cz>
2024-10-12 22:14:31 +0200

    Init DAL

---
+++ DAL/Data/BookHubDBContext.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data;
+
+public class BookHubDBContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    public BookHubDBContext(DbContextOptions<BookHubDBContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.SetNull;
+        }
+
+        modelBuilder.Seed();
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ DAL/Data/DataInitializer.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public static class DataInitializer
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            var books = PrepareBookModels();
+
+            modelBuilder.Entity<Book>()
+                .HasData(books);
+        }
+
+        private static List<Book> PrepareBookModels()
+        {
+            return new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Cesta do neznáma"
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Hadí princezna a 102 psů"
+                },
+            };
+        }
+    }
+}

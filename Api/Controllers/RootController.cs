374225b38af21ff23407c00cede322ac85c51f1e
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-10-20 13:32:00 +0200

    feat: add authorization middleware

--- Api/Controllers/RootController.cs
+++ Api/Controllers/RootController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
     [ApiController]
     [Route("/")]
+    [AllowAnonymous]
     public class RootController(ILogger<RootController> logger) : ControllerBase
     {
         [HttpGet()]

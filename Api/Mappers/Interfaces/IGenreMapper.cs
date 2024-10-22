beb4a7fdb2852ffd34caa35b926ba6c6385fdf7f
Patrik Korytar <patrik.korytar@gmail.com>
2024-10-22 17:55:20 +0200

    feat: implement /genres endpoints

---
+++ Api/Mappers/Interfaces/IGenreMapper.cs
@@ -0,0 +1,7 @@
+using Api.DTOs.Genre;
+using DAL.Models;
+
+namespace Api.Mappers.Interfaces;
+
+public interface IGenreMapper
+    : IEntityMapper<Genre, GenreDto, GenreDetailDto, GenreCreateDto, GenreUpdateDto> { }

f348be763772a86e800552e4f286641b44cfc176
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-11-16 15:45:20 +0100

    feat: add hello world test

---
+++ BusinessLayer.Tests/Services/HelloWorldTest.cs
@@ -0,0 +1,12 @@
+namespace BusinessLayer.Tests;
+
+public class HelloWorldTest
+{
+    public HelloWorldTest() { }
+
+    [Fact]
+    public void GetUsersAsync_ExactMatch()
+    {
+        Assert.Equal(4, 4);
+    }
+}
---
+++ BusinessLayer.Tests/Usings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.EntityFrameworkCore;
+/*global using Moq;*/
+global using Xunit;

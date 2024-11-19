89b7de2b91d03c54602fc469b916c772beef9c2a
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-11-19 12:02:39 +0100

    refactor: fix roslynator errors

--- TestUtilities/Data/TestDataHelper.cs
+++ TestUtilities/Data/TestDataHelper.cs
@@ -6,8 +6,8 @@ public static class TestDataHelper
 {
     public static List<Author> GetFakeAuthors()
     {
-        return new List<Author>
-        {
+        return
+        [
             new Author()
             {
                 Id = 1,
@@ -26,22 +26,22 @@ public static class TestDataHelper
                 Name = "John",
                 Surname = "Smith",
             },
-        };
+        ];
     }
 
     public static List<Publisher> GetFakePublishers()
     {
-        return new List<Publisher>
-        {
+        return
+        [
             new Publisher { Id = 1, Name = "Penguin s.r.o" },
             new Publisher { Id = 2, Name = "Panta Rhei Books" },
-        };
+        ];
     }
 
     public static List<Book> GetFakeBooks()
     {
-        return new List<Book>
-        {
+        return
+        [
             new Book
             {
                 Name = "Book1",
@@ -78,7 +78,7 @@ public static class TestDataHelper
                 PreviewImageId = "1849645247",
                 Rating = 5.0,
             },
-        };
+        ];
     }
 
     public static List<BookImage> GetFakeBookImage()
@@ -96,38 +96,38 @@ public static class TestDataHelper
 
     public static List<Genre> GetFakeGenres()
     {
-        return new List<Genre>
-        {
+        return
+        [
             new Genre { Id = 1, GenreType = "Fantasy" },
             new Genre { Id = 2, GenreType = "Science Fiction" },
             new Genre { Id = 3, GenreType = "Horror" },
-        };
+        ];
     }
 
     public static List<BookGenre> GetFakeBookGenres()
     {
-        return new List<BookGenre>
-        {
+        return
+        [
             new BookGenre { BookId = 1, GenreId = 1 },
             new BookGenre { BookId = 2, GenreId = 2 },
             new BookGenre { BookId = 3, GenreId = 3 },
-        };
+        ];
     }
 
     public static List<BookAuthor> GetFakeBookAuthors()
     {
-        return new List<BookAuthor>
-        {
+        return
+        [
             new BookAuthor { BookId = 1, AuthorId = 1 },
             new BookAuthor { BookId = 2, AuthorId = 2 },
             new BookAuthor { BookId = 3, AuthorId = 3 },
-        };
+        ];
     }
 
     public static List<User> GetFakeUsers()
     {
-        return new List<User>
-        {
+        return
+        [
             new User
             {
                 Id = 1,
@@ -142,47 +142,44 @@ public static class TestDataHelper
                 Email = "user@aaa.cz",
                 Role = UserRole.User,
             },
-        };
+        ];
     }
 
     public static List<WishListItem> GetFakeWishListItems()
     {
-        return new List<WishListItem>
-        {
-            new WishListItem { UserId = 2, BookId = 3 },
-        };
+        return [new WishListItem { UserId = 2, BookId = 3 }];
     }
 
     public static List<Order> GetFakeOrders()
     {
-        return new List<Order>
-        {
+        return
+        [
             new Order
             {
                 Id = 1,
                 UserId = 2,
                 TotalPrice = 20.5,
             },
-        };
+        ];
     }
 
     public static List<OrderItem> GetFakeOrderItems()
     {
-        return new List<OrderItem>
-        {
+        return
+        [
             new OrderItem
             {
                 OrderId = 1,
                 BookId = 1,
                 Quantity = 1,
             },
-        };
+        ];
     }
 
     public static List<Review> GetFakeBookReviews()
     {
-        return new List<Review>
-        {
+        return
+        [
             new Review
             {
                 BookId = 1,
@@ -204,6 +201,6 @@ public static class TestDataHelper
                 Rating = 5,
                 Body = "Perfect book",
             },
-        };
+        ];
     }
 }

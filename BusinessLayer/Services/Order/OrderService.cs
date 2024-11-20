71bacc79e5c64fd8b9eb67551016871bdcbca764
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-11-20 11:17:36 +0100

    feat: implement orderService

---
+++ BusinessLayer/Services/Order/OrderService.cs
@@ -0,0 +1,88 @@
+using BusinessLayer.DTOs.Order;
+using BusinessLayer.Mappers.Interfaces;
+using BusinessLayer.Services.Order.Interfaces;
+using DAL.Data;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLayer.Services.Order;
+
+public class OrderService(BookHubDBContext dBContext, IOrderMapper orderMapper)
+    : BaseService(dBContext),
+        IOrderService
+{
+    public async Task<List<OrderDto>> GetAllOrdersAsync()
+    {
+        var orders = await dBContext.Orders.ToListAsync();
+
+        return orders.ConvertAll(orderMapper.ToDto);
+    }
+
+    public async Task<OrderDetailDto?> GetSingleOrderAsync(int orderId)
+    {
+        var order = await dBContext.Orders.FindAsync(orderId);
+        if (order == null)
+        {
+            return null;
+        }
+
+        return orderMapper.ToDetailDto(order);
+    }
+
+    public async Task<OrderDto?> CreateOrderAsync(OrderCreateDto orderCreateDto, int userId)
+    {
+        await using var transaction = await dBContext.Database.BeginTransactionAsync();
+        try
+        {
+            double totalPrice = 0;
+            foreach (var oi in orderCreateDto.OrderItems)
+            {
+                var bookPrice = await GetBookPrice(oi.BookId);
+                totalPrice += bookPrice * oi.Quantity;
+            }
+
+            var order = new DAL.Models.Order { UserId = userId, TotalPrice = totalPrice };
+
+            await dBContext.Orders.AddAsync(order);
+
+            await SaveAsync(true);
+
+            var orderItems = new List<OrderItem>();
+            foreach (var oi in orderCreateDto.OrderItems)
+            {
+                var bookPrice = await GetBookPrice(oi.BookId);
+                orderItems.Add(
+                    new OrderItem
+                    {
+                        OrderId = order.Id,
+                        BookId = oi.BookId,
+                        Quantity = oi.Quantity,
+                        PricePerItem = bookPrice,
+                    }
+                );
+            }
+
+            dBContext.OrderItem.AddRange(orderItems);
+
+            await SaveAsync(true);
+
+            await transaction.CommitAsync();
+
+            return orderMapper.ToDto(order);
+        }
+        catch (Exception)
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+    }
+
+    private async Task<double> GetBookPrice(int bookId)
+    {
+        var book =
+            await dBContext.Books.FindAsync(bookId)
+            ?? throw new InvalidOperationException("Book not found");
+
+        return book.Price;
+    }
+}

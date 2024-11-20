6738d56574ef84e0c63113edd63199c7c6dfc3b0
Machala, Oldřich <oldrich.machala@thermofisher.com>
2024-11-20 18:35:31 +0100

    fix: catch and log error if elasticsearch is missing

--- Api/Middlewares/RequestLoggingMiddleware.cs
+++ Api/Middlewares/RequestLoggingMiddleware.cs
@@ -3,7 +3,7 @@ using Elastic.Clients.Elasticsearch;
 
 namespace Api.Middlewares;
 
-public class RequestLoggingMiddleware(RequestDelegate next, ElasticsearchClient elasticClient)
+public class RequestLoggingMiddleware(RequestDelegate next, ElasticsearchClient elasticClient, ILogger<RequestLoggingMiddleware> logger)
 {
     public async Task Invoke(HttpContext context)
     {
@@ -33,8 +33,14 @@ public class RequestLoggingMiddleware(RequestDelegate next, ElasticsearchClient 
             var duration = DateTime.UtcNow - startTime;
             logEntry.Response.StatusCode = context.Response.StatusCode;
             logEntry.DurationMilliseconds = duration.TotalMilliseconds;
-
-            await elasticClient.IndexAsync(logEntry, i => i.Index("logs"));
+            try
+            {
+                await elasticClient.IndexAsync(logEntry, i => i.Index("logs"));
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e.Message);
+            }
         }
     }
 }

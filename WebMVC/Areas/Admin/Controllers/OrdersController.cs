c108626d0ef25ed0650e4e37b72c59ee4c8671f3
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-12-09 16:18:08 +0100

    feat: add other controllers

---
+++ WebMVC/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.ViewModels;
+
+namespace WebMVC.Area.Admin.Controllers;
+
+public class OrdersController(ILogger<OrdersController> logger) : AdminController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}

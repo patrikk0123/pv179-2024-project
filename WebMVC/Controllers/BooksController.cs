886491b826aa41b9927030375291546f4c37f0aa
Lukco, Marcel <Marcel.Lukco@travelport.com>
2024-12-10 14:54:10 +0100

    feat: implement book detail page

--- WebMVC/Controllers/BooksController.cs
+++ WebMVC/Controllers/BooksController.cs
@@ -1,11 +1,20 @@
+using BusinessLayer.DTOs.BookReview;
 using BusinessLayer.Services.Book.Interfaces;
+using BusinessLayer.Services.BookReview.Interfaces;
+using DAL.Models.Auth;
 using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebMVC.ViewModels.Book;
 
 namespace WebMVC.Controllers;
 
-public class BooksController(IBookService bookService) : Controller
+public class BooksController(
+    IBookService bookService,
+    IBookReviewService bookReviewService,
+    UserManager<LocalIdentityUser> userManager
+) : Controller
 {
     [HttpGet("")]
     public async Task<IActionResult> GetAllBooks()
@@ -13,6 +22,67 @@ public class BooksController(IBookService bookService) : Controller
         var books = await bookService.GetAllBooksAsync(null, null, null, null, null, null);
 
         var model = books.ConvertAll(book => book.Adapt<BookViewModel>());
+
+        return View(model);
+    }
+
+    [HttpGet("detail/{bookId}")]
+    public async Task<IActionResult> GetDetailBook(int bookId)
+    {
+        var book = await bookService.GetSingleBookAsync(bookId);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        var model = book.Adapt<BookDetailViewModel>();
+
+        return View(model);
+    }
+
+    [HttpPost("detail/{bookId}")]
+    [Authorize]
+    public async Task<IActionResult> GetDetailBook(int bookId, BookReviewModel modelReview)
+    {
+        var book = await bookService.GetSingleBookAsync(bookId);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        var user = await userManager.GetUserAsync(User);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await bookReviewService.CreateSingleReviewAsync(
+                    bookId,
+                    user.UserId,
+                    new BookReviewCreateDto()
+                    {
+                        Rating = modelReview.Rating,
+                        Body = modelReview.Body,
+                    }
+                );
+
+                book = await bookService.GetSingleBookAsync(bookId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ModelState.AddModelError(string.Empty, "Something wrong occured.");
+            }
+        }
+
+        var model = book.Adapt<BookDetailViewModel>();
 
         return View(model);
     }
---
+++ WebMVC/ViewModels/Book/BookDetailViewModel.cs
@@ -0,0 +1,32 @@
+using BusinessLayer.DTOs.BookReview;
+
+namespace WebMVC.ViewModels.Book;
+
+public class BookDetailViewModel
+{
+    public required int Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Description { get; init; }
+
+    public required int Pages { get; init; }
+
+    public required string ISBN { get; init; }
+
+    public required DateOnly PublishDate { get; init; }
+
+    public required string PublisherName { get; init; }
+
+    public required List<String> Genres { get; init; } = [];
+
+    public required List<BookReviewDto> Reviews { get; init; } = [];
+
+    public required double Rating { get; init; }
+
+    public required double Price { get; init; }
+
+    public byte[] PreviewImage { get; init; }
+
+    public required List<String> Authors { get; init; } = [];
+}

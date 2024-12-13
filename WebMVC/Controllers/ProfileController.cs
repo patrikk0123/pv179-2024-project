ed52a4d27eef9f9e3b3129f8de2aa586a6bddb97
Patrik Korytar <patrik.korytar@gmail.com>
2024-12-13 16:42:42 +0100

    feat: implement profile detail and edit pages

--- WebMVC/Controllers/ProfileController.cs
+++ WebMVC/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 using BusinessLayer.Services.Order.Interfaces;
+using BusinessLayer.Services.User.Interfaces;
 using BusinessLayer.Services.WishList.Interfaces;
 using DAL.Models.Auth;
 using Mapster;
@@ -6,16 +7,72 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebMVC.ViewModels.Order;
+using WebMVC.ViewModels.User;
 using WebMVC.ViewModels.WishListItem;
 
 namespace WebMVC.Controllers;
 
 public class ProfileController(
     UserManager<LocalIdentityUser> userManager,
+    IUserService userService,
     IWishlistService wishlistService,
     IOrderService orderService
 ) : Controller
 {
+    [HttpGet("/profile")]
+    [Authorize]
+    public async Task<IActionResult> ProfileDetail()
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        var userDto = await userService.GetSingleUserAsync(user.UserId);
+
+        var model = userDto.Adapt<UserViewModel>();
+
+        return View(model);
+    }
+
+    [HttpGet("/profile/update")]
+    [Authorize]
+    public async Task<IActionResult> ProfileUpdate()
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        var userDto = await userService.GetSingleUserAsync(user.UserId);
+
+        var model = userDto.Adapt<UserUpdateViewModel>();
+
+        return View(model);
+    }
+
+    [HttpPost("/profile/update")]
+    [Authorize]
+    public async Task<IActionResult> ProfileUpdate(UserUpdateViewModel model)
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        if (ModelState.IsValid)
+        {
+            user.UserName = model.Username;
+            user.Email = model.Email;
+            user.User.Username = model.Username;
+            user.User.Email = model.Email;
+            var result = await userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(ProfileDetail), "Profile");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        return View(model);
+    }
+
     [HttpGet("/profile/wishlist")]
     [Authorize]
     public async Task<IActionResult> WishList()

df10fcc3065ad2f07cfc3ab46fc5d5a0315866a3
Patrik Korytar <patrik.korytar@gmail.com>
2024-12-14 16:32:11 +0100

    feat: add Admin Order list, detail and update pages

--- WebMVC/Areas/Admin/Controllers/OrdersController.cs
+++ WebMVC/Areas/Admin/Controllers/OrdersController.cs
@@ -1,11 +1,62 @@
+using BusinessLayer.DTOs.Order;
+using BusinessLayer.Services.Order.Interfaces;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
+using WebMVC.Areas.Admin.ViewModels.Orders;
+using WebMVC.ViewModels.Order;
 
 namespace WebMVC.Areas.Admin.Controllers;
 
-public class OrdersController(ILogger<OrdersController> logger) : AdminController
+public class OrdersController(IOrderService orderService) : AdminController
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var orders = await orderService.GetAllOrdersAsync();
+
+        var model = orders.Adapt<OrderListViewModel>();
+
+        return View(model);
+    }
+
+    [HttpGet("admin/orders/{orderId}")]
+    public async Task<IActionResult> Detail(int orderId)
+    {
+        var order = await orderService.GetSingleOrderAsync(orderId);
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        var model = order.Adapt<OrderDetailViewModel>();
+
+        return View(model);
+    }
+
+    [HttpGet("admin/orders/update/{orderId}")]
+    public async Task<IActionResult> Update(int orderId)
+    {
+        var order = await orderService.GetSingleOrderAsync(orderId);
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        var model = order.Adapt<OrderUpdateViewModel>();
+
+        return View(model);
+    }
+
+    [HttpPost("admin/orders/update/{orderId}")]
+    public async Task<IActionResult> Update(int orderId, OrderUpdateViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var orderUpdateDto = model.Adapt<OrderUpdateDto>();
+            await orderService.UpdateSingleOrderAsync(orderId, orderUpdateDto);
+
+            return RedirectToAction("Index");
+        }
+
+        return View(model);
     }
 }
